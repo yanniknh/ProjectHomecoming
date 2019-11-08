447cd3bdc8851aee14d79279696c700ca96f4d1d
TheMultiZero <hs.multizero@gmail.com>
2019-11-09 00:42:18 +0100

    added more routes for registration

--- frontend/project-homecoming/src/app/app-routing.module.ts
+++ frontend/project-homecoming/src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
 import { SelectionPageComponent } from './pages/selection-page/selection-page.component';
+import { PersonalInformationRegistrationComponent } from './components/personal-information-registration/personal-information-registration.component';
+import { PreferenceSelectionComponent } from './components/preference-selection/preference-selection.component';
 
 const routes: Routes = [
 { path: 'welcome', component: WelcomePageComponent},
-{ path: 'selectionPage', component: SelectionPageComponent}
+{ path: 'selectionPage', component: SelectionPageComponent},
+{ path: 'personalInfo', component: PersonalInformationRegistrationComponent},
+{ path: 'preferenceSelection', component: PreferenceSelectionComponent}
 ];
 
 @NgModule({
--- frontend/project-homecoming/src/app/pages/selection-page/selection-page.component.ts
+++ frontend/project-homecoming/src/app/pages/selection-page/selection-page.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectionPageComponent implements OnInit {
 
+  status: String;
+
   constructor() { }
 
   ngOnInit() {
+    this.status = '!isOn';
   }
+
 
 }
