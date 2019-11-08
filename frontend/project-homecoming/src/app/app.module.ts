20546f2ea349055ed9da1f6e0769945312716dab
disoslei <felix@Felixs-MacBook-Pro.local>
2019-11-08 23:49:02 +0100

    added components

--- frontend/project-homecoming/src/app/app.module.ts
+++ frontend/project-homecoming/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
 import { SelectionPageComponent } from './pages/selection-page/selection-page.component';
+import { PreferenceSelectionComponent } from './components/preference-selection/preference-selection.component';
+import { PersonalInformationRegistrationComponent } from './components/personal-information-registration/personal-information-registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomePageComponent,
     SelectionPageComponent,
+    PreferenceSelectionComponent,
+    PersonalInformationRegistrationComponent,
   ],
   imports: [
     BrowserModule,
---
+++ frontend/project-homecoming/src/app/components/personal-information-registration/personal-information-registration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-information-registration',
+  templateUrl: './personal-information-registration.component.html',
+  styleUrls: ['./personal-information-registration.component.css']
+})
+export class PersonalInformationRegistrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
