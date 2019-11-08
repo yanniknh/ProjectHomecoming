8864bb15d71cc5d87853b604a8e22412241b9b4f
Voss <lvoss@s-und-n.de>
2019-11-08 21:28:07 +0100

    initial commit

---
+++ backend/src/main/java/de/homecoming/projecthomecoming/ProjectHomecomingApplication.java
@@ -0,0 +1,13 @@
+package de.homecoming.projecthomecoming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectHomecomingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectHomecomingApplication.class, args);
+	}
+
+}
