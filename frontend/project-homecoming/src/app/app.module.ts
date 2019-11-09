3f0c3a38183d2828fea5cf3fed4c61aaa57b6d28
Meik <meiksar@Meiks-MBP.fritz.box>
2019-11-09 14:17:50 +0100

    Current Status

--- frontend/project-homecoming/src/app/app.module.ts
+++ frontend/project-homecoming/src/app/app.module.ts
@@ -2,11 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
 import { SelectionPageComponent } from './pages/selection-page/selection-page.component';
 import { PreferenceSelectionComponent } from './components/preference-selection/preference-selection.component';
 import { PersonalInformationRegistrationComponent } from './components/personal-information-registration/personal-information-registration.component';
+import {
+  MatButtonModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatCardModule,
+  MatTableModule
+} from "@angular/material";
 
 @NgModule({
   declarations: [
@@ -18,7 +30,17 @@ import { PersonalInformationRegistrationComponent } from './components/personal-
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatCardModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/project-homecoming/src/app/pages/selection-page/selection-page.component.ts
+++ frontend/project-homecoming/src/app/pages/selection-page/selection-page.component.ts
@@ -1,19 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+import { trigger, keyframes, animate, transition } from '@angular/animations';
+import * as kf from './keyframes';
+
+var object =document.getElementById('object');
+
+/*
 @Component({
   selector: 'app-selection-page',
   templateUrl: './selection-page.component.html',
-  styleUrls: ['./selection-page.component.css']
+  styleUrls: ['./selection-page.component.css'],
+  animations: [
+    trigger('cardAnimator', [
+      transition('* => wobble', animate(1000, keyframes(kf.wobble))),
+      transition('* => swing', animate(1000, keyframes(kf.swing))),
+      transition('* => jello', animate(1000, keyframes(kf.jello))),
+      transition('* => zoomOutRight', animate(1000, keyframes(kf.zoomOutRight))),
+      transition('* => slideOutLeft', animate(1000, keyframes(kf.slideOutLeft))),
+      transition('* => rotateOutUpRight', animate(1000, keyframes(kf.rotateOutUpRight))),
+      transition('* => flipOutY', animate(1000, keyframes(kf.flipOutY))),
+    ])
+  ]
 })
-export class SelectionPageComponent implements OnInit {
+*/
 
-  status: String;
+var swipe = new Hammer(object);
 
-  constructor() { }
+swipe.on('tap', function(ev) {
+  console.log('Tap');
+})
 
-  ngOnInit() {
-    this.status = '!isOn';
+export class SelectionPageComponent {
+
+  animationState: string;
+
+  startAnimation(state) {
+    console.log(state)
+    if (!this.animationState) {
+      this.animationState = state;
+    }
   }
 
+  resetAnimationState() {
+    this.animationState = '';
+  }
 
 }
